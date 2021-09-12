20816bb87dd992e2c72269e02654c50bdf96c1aa
RyosukeSomeya <rx.78.0618@gmail.com>
2021-09-13 00:30:06 +0900

    model正常系テスト実装:1

---
+++ spec/models/food_enquete_spec.rb
@@ -0,0 +1,39 @@
+require 'rails_helper'
+
+RSpec.describe FoodEnquete, type: :model do
+  describe '正常系の機能' do
+    context '回答する' do
+      it '正しく登録できること 料理:やきそば food_id: 2,
+      満足度:良い score: 3,
+      希望するプレゼント:ビール飲み放題 present_id: 1)' do
+        # Test data
+        enquete = FoodEnquete.new(
+          name: '田中 太郎',
+          mail: 'taro.tanaka@example.com',
+          age: 25,
+          food_id: 2,
+          score: 3,
+          request: 'おいしかったです。',
+          present_id: 1
+        )
+
+        # バリデーション検証
+        expect(enquete).to be_valid
+
+        # データ保存
+        enquete.save
+
+        # データ取得
+        answered_enquete = FoodEnquete.find(1)
+
+        expect(answered_enquete.name).to eq('田中 太郎')
+        expect(answered_enquete.mail).to eq('taro.tanaka@example.com')
+        expect(answered_enquete.age).to eq(25)
+        expect(answered_enquete.food_id).to eq(2)
+        expect(answered_enquete.score).to eq(3)
+        expect(answered_enquete.request).to eq('おいしかったです。')
+        expect(answered_enquete.present_id).to eq(1)
+      end
+    end
+  end
+end
